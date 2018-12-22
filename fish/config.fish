2c374f5233ff93576d8e64a48ebc0f71656d32fc
Ryan Ackermann <ryan@ackermann.io>
2018-12-21 22:23:53 -0600

    updated my fish config
    to support go module execution (govendor)

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 cd ~/Developer
 set -g -x fish_greeting ''
-set -x GOPATH $HOME/Developer/golang
-set -x PATH $HOME/.fastlane/bin $PATH
 set -x PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+set -x PATH $HOME/.fastlane/bin $PATH
+
+set -x GOPATH $HOME/Developer/golang
+set -x PATH $GOPATH/bin $PATH
 
 alias edit="open -a /Applications/CodeRunner.app"
 
